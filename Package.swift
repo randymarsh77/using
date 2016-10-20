e2c6d897e8f1b22b45e5fddd78f82872946b0c83
Matthew Preucil <mattpsbs@gmail.com>
2016-10-19 18:36:45 -0700

    Fix package reference

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Using",
     dependencies: [
-	.Package(url: "https://www.github.com/randymarsh77/idisposable.git", majorVersion: 1)
+	.Package(url: "https://www.github.com/randymarsh77/idisposable", majorVersion: 1)
 	]
 )
