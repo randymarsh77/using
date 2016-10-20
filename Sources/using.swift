b7c643fb36443b53ad98452a5bed7f135d751ba5
Matthew Preucil <mattpsbs@gmail.com>
2016-10-20 01:48:09 -0700

    Add overloads for accessing the disposable

--- Sources/using.swift
+++ Sources/using.swift
@@ -11,3 +11,15 @@ public func using(_ disposable: IDisposable, closure: () throws -> ())
 	defer { disposable.dispose() }
 	try? closure()
 }
+
+public func using<T: IDisposable>(_ disposable: IDisposable, closure: (_ arg: T) -> ())
+{
+	closure(disposable as! T)
+	disposable.dispose()
+}
+
+public func using<T: IDisposable>(_ disposable: IDisposable, closure: (_ arg: T) throws -> ())
+{
+	defer { disposable.dispose() }
+	try? closure(disposable as! T)
+}
