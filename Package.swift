b346f42329b2c3c33a89929c3cf54696037c2ab4
Matthew Preucil <mattpsbs@gmail.com>
2016-11-21 19:48:34 -0800

    Remove www from Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Using",
     dependencies: [
-	.Package(url: "https://www.github.com/randymarsh77/idisposable", majorVersion: 1)
-	]
+	.Package(url: "https://github.com/randymarsh77/idisposable", majorVersion: 1)
+    ]
 )
