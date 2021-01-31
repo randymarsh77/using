048648b21b6d73e9d5cfee62ff7ee29d74dbcd92
randymarsh77 <mattpsbs@gmail.com>
2021-01-31 09:32:20 -0800

    Update to Swift 5.1.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,21 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "Using",
-    dependencies: [
-	.Package(url: "https://github.com/randymarsh77/idisposable", majorVersion: 1)
-    ]
+	name: "Using",
+	products: [
+		.library(
+			name: "Using",
+			targets: ["Using"]
+		),
+	],
+	dependencies: [
+		.package(url: "https://github.com/randymarsh77/idisposable", .branch("master")),
+	],
+	targets: [
+		.target(
+			name: "Using",
+			dependencies: ["IDisposable"]
+		),
+	]
 )
